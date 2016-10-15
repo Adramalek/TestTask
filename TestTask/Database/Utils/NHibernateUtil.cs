c889d1607a8ce56e7f47fa54a8f50f06418b3f8b
Adramalek <beherith10.02@gmail.com>
2016-10-15 18:08:46 +0300

    SessionFactory generator.

---
+++ TestTask/Database/Utils/NHibernateUtil.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using NHibernate;
+using NHibernate.Cfg;
+
+namespace TestTask.Database.Util
+{
+	public class NHibernateUtil
+	{
+		private static ISessionFactory factory;
+
+		static NHibernateUtil()
+		{
+			Configuration cfg = new Configuration();
+			cfg.AddAssembly(Assembly.GetExecutingAssembly());
+			cfg.Configure();
+			Factory = cfg.BuildSessionFactory();
+		}
+
+		public static ISessionFactory Factory
+		{
+			get { return factory; }
+			private set { factory = value; }
+		}
+	}
+}
