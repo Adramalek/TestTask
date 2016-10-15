d09b6911a01c0af8983a1999dc2c153043672c3b
Adramalek <beherith10.02@gmail.com>
2016-10-15 18:02:04 +0300

    Representation of EncryptionData table. Subclass of AEntity.

---
+++ TestTask/Database/Entities/EncryptionData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TestTask.Database.Entity
+{
+	public class EncryptionData : AEntity
+	{
+		private string oldSymbol;
+		private string newSymbol;
+
+		public virtual string OldSymbol { get; set; }
+		public virtual string NewSymbol { get; set; }
+	}
+}
