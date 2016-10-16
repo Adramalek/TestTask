12076076a32e02be23809c9c6c606c09032cbb07
Adramalek <beherith10.02@gmail.com>
2016-10-16 15:41:00 +0300

    Common, event based validator.

---
+++ TestTask/Utils/Validation/Validator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TestTask.Util;
+
+namespace TestTask.Util.Validation
+{
+	public class Validator<T> where T : class
+	{
+		public event FailEvent onFail = delegate { };
+		public event AcceptEvent onAccept = delegate { };
+
+		public bool Validate(T arg, params T[] correctVariants)
+		{
+			bool correct = false;
+			int var = 0;
+			if (correctVariants != null && correctVariants.Length != 0)
+			{
+				for (int i = 0; i < correctVariants.Length; i++)
+				{
+					if (arg.Equals(correctVariants[i]))
+					{
+						correct = true;
+						var = i;
+						break;
+					}
+				}
+			}
+			else correct = true;
+			if (!correct) onFail();
+			else onAccept(var);
+			return correct;
+		}
+	}
+}
